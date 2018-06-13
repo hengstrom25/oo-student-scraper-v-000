4b42e92d108968cd43065dd13d2bd91621aaebaa
Heidi Engstrom <hengstrom25@gmail.com>
2018-06-13 19:17:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile = Nokogiri::HTML(open(profile_url))
     students_profile = {}
-    social_media = profile.css("div.social-icon-container a").collect {|a| a.attribute("href").value)
+    social_media = profile.css("div.social-icon-container a").collect {|a| a.attribute("href").value}
     social_media.each do |link|
       case
       when link.include?("twitter")
